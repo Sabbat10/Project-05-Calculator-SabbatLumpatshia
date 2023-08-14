8ebca5b427d6f2ee961b8e07f9042b4beb47be9f
Sabbat10 <sabbatlumpatshia99@gmail.com>
2023-08-14 10:15:37 +0100

    add select elements

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
-{ calculate } import './calculator';
+// { calculate } import './calculator';
 
 // TODO: Faire la manipulation du DOM dans ce fichier
+
+
+// Eléments 
+const calcul = document.getElementById('calcul');
+const inputButton = document.getElementById('input');
+
+const digitButton = document.getElementById('digit');
+const theOperator = document.querySelectorAll('#minus, #times, #divideby, #plus');
+
+const clearButton = document.getElementById('clear');
+const reset = document.getElementById('reset');
+
+const dotNumpad = document.getElementsByClassName('numpad dot');
+const plusoumoins = document.getElementById('plusoumoins');
+
+console.log(resetButton);
