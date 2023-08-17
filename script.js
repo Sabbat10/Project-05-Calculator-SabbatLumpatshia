78d3d39cfd85b85256ee7340ef1feca9c4f75737
Sabbat10 <sabbatlumpatshia99@gmail.com>
2023-08-17 10:41:34 +0100

    more mofication

--- script.js
+++ script.js
@@ -3,93 +3,71 @@
 // TODO: Faire la manipulation du DOM dans ce fich
 
 // seclect elements 
-const inputButton = document.getElementById('input');
-const calcul = document.getElementById('calcul');
+// Sélectionner tous les boutons numériques et leur ajouter un écouteur d'événement
+const digitButtons = document.querySelectorAll(".numpad.digit");
+digitButtons.forEach((button) => {
+  button.addEventListener("click", myDigit);
+});
 
-const digits = document.getElementsByClassName('digit');
-const theOperator = document.querySelectorAll('#minus, #times, #divideby, #plus');
+// Sélectionner tous les boutons d'opérateur et leur ajouter un écouteur d'événement
+const operatorButtons = document.querySelectorAll("#plus, #minus, #times, #divideby");
+operatorButtons.forEach((button) => {
+  button.addEventListener("click", toLabel);
+});
 
-const clearButton = document.getElementById('clear');
-const resetButton = document.getElementById('reset');
+// Sélectionner le bouton "C" (clear) et lui ajouter un écouteur d'événement
+const clearButton = document.getElementById("clear");
+clearButton.addEventListener("click", clearInput);
 
-const numpadDot = document.getElementsByClassName('numpad dot');
-const plusoumoins = document.getElementById('plusoumoins');
+// Sélectionner le bouton "AC" (reset) et lui ajouter un écouteur d'événement
+const resetButton = document.getElementById("reset");
+resetButton.addEventListener("click", reset);
 
-// add style
-calcul.style.maxWidth = "400px"
-calcul.style.maxHeight = "auto"
-calcul.style.margin = "auto"
-console.log(calcul.style);  
-
-// change event types
-let newtheOperator = [];
-for (let i = 0; i < theOperator.length; i++) {
-    const element = theOperator[i];
-    element.type = "button";
-    newtheOperator.push(element);
-}
-
-
-// event function
-function myDigit() {
-    inputButton.value += this.innerText;
-}
-
-function toLabel() {
-    calcul.innerText += ' ' + inputButton.value + ' ' + this.innerText;
-    inputButton.value = "";
-    inputButton.name = "";
-}
+// Sélectionner le bouton "+/-" (changeSign) et lui ajouter un écouteur d'événement
+const plusoumoins = document.getElementById("plusoumoins");
+plusoumoins.addEventListener('click', changeSign);
 
 function clickZero() {
-    if (inputButton.value === "") {
-        inputButton.value = "";
-    }else{
-        inputButton.value += this.innerText;
-    }
+    const input = document.getElementById("input");
+    input.value += "0";
 }
 
-function changeSign() {
-    let valeur = Array.from(inputButton.value);
-   
-    if (!inputButton.value) {
-        inputButton.value;
-    }else if (inputButton.value && valeur.at(0)!=="-"){
-        valeur.unshift("-");
-        inputButton.value =(valeur.join(""));
-    }else if (inputButton.value && valeur.at(0)==="-"){
-        valeur.shift();
-        inputButton.value=(valeur.join("")) ;
-    }else{
-        inputButton.value;
-    }
+function myDigit(event) {
+    const digit = event.target.innerText;
+    const input = document.getElementById("input");
+    input.value += digit;
 }
 
-function reset() {
-    calcul.innerText = "";
-    inputButton.value = "";
+function toLabel(event) {
+    const operator = event.target.innerText;
+    const input = document.getElementById("input");
+    
+    // Vérifier si un opérateur existe déjà dans l'entrée utilisateur
+    if (!/[+\-*/]$/.test(input.value)) { 
+        // Ajouter seulement si aucun opérateur n'est déjà présent
+        input.value += ` ${operator} `;
+    }
 }
 
 function clearInput() {
-    inputButton.value = "";
+    const input = document.getElementById("input");
+    input.value = "";
 }
 
-//add events
+function reset() {
+    const input = document.getElementById("input");
+    const calculationParagraph = document.getElementById("calcul");
+  
+    input.value = "";
+    
+    // Remplacer le texte du paragraphe de calcul avec un texte vide
+    calculationParagraph.innerText = "";
+}
 
-for (let index = 0; index < digits.length; index++) {
-    const element = digits[index];
-    if (element.innerText === "0") {
-        element.addEventListener('click', clickZero);
-    }else{
-        element.addEventListener('click', myDigit)
+function changeSign() {
+    const input = document.getElementById("input");
+    if (parseFloat(input.value) !==0) { 
+      // Vérifier que l'entrée n'est pas déjà zéro pour éviter les erreurs d'exécution.
+      input.value = parseFloat(input.value) * -1;
     }
 }
-
-for (let i = 0; i < theOperator.length; i++) {
-    const element = theOperator[i];
-    element.addEventListener('click', toLabel);
-}
-
-clearButton.addEventListener('click',clearInput);
-resetButton.addEventListener('click', reset); 
-plusoumoins.addEventListener('click', changeSign);
